a48502dc8dcb6f1ee223f378900c033a19363903
David Gaspar <davidgaspar.dev@gmail.com>
2022-08-24 22:16:25 -0300

    test: add route with param

--- examples/main.go
+++ examples/main.go
@@ -13,10 +13,20 @@ func main() {
 	server.SetLogger(true)
 
 	server.AddRoute("GET", "/v1/hello", []middleware.Middleware{}, HelloWorld)
+	server.AddRoute("GET", "/v1/hello/:name", nil, HelloWithParam)
 
 	server.Run()
 }
 
 func HelloWorld(req *helpers.Request, res *helpers.Response) {
 	res.Ok("hello world")
+}
+
+func HelloWithParam(req *helpers.Request, res *helpers.Response) {
+	data := map[string]interface{}{
+		"name": req.GetParam("name"),
+		"type": req.GetContentType(),
+	}
+
+	res.Ok(data)
 }
